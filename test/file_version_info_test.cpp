2b8fdaa4bc57d06017f4995f623beff0ac836ca3
Joshua Napoli <jnapoli@alum.mit.edu>
2014-01-05 15:29:39 -0500

    Rename the module class to module_type.

--- test/file_version_info_test.cpp
+++ test/file_version_info_test.cpp
@@ -12,17 +12,17 @@
 #include <boost/test/unit_test.hpp>
 #include <casements/file_version_info.hpp>
 #include <casements/version_type.hpp>
-#include <windows/module.hpp>
+#include <windows/module_type.hpp>
 
 BOOST_AUTO_TEST_CASE(should_read_file_version_info)
 {
-  windows::module current_process;
+  windows::module_type current_process;
   BOOST_CHECK_NO_THROW(casements::file_version_info(current_process.filename()))
 }
 
 BOOST_AUTO_TEST_CASE(should_get_version)
 {
-  windows::module current_process;
+  windows::module_type current_process;
   casements::file_version_info file_version_info(current_process.filename());
   casements::version_type version(file_version_info.version());
   BOOST_CHECK_EQUAL(version, casements::version_type(1,2,3,4));
@@ -30,14 +30,14 @@ BOOST_AUTO_TEST_CASE(should_get_version)
 
 BOOST_AUTO_TEST_CASE(should_get_company_name)
 {
-  windows::module current_process;
+  windows::module_type current_process;
   casements::file_version_info file_version_info(current_process.filename());
-  BOOST_CHECK_EQUAL(file_version_info.company_name(), "DTI3D");
+  BOOST_CHECK_EQUAL(file_version_info.company_name(), "Napoli Visual Software");
 }
 
 BOOST_AUTO_TEST_CASE(should_get_product_name)
 {
-  windows::module current_process;
+  windows::module_type current_process;
   casements::file_version_info file_version_info(current_process.filename());
-  BOOST_CHECK_EQUAL(file_version_info.product_name(), "windows_test");
+  BOOST_CHECK_EQUAL(file_version_info.product_name(), "casement_test");
 }
